5fae6bcc6b49603ffbf7116dc957bb18804a0e14
Sean Stoves <sean@seanstoves.com>
2025-10-06 10:10:52 -0400

    undo 404 alterations

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -5,12 +5,6 @@ import type { Database } from './types';
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const SUPABASE_PUBLISHABLE_KEY = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
-console.log('ENV VARIABLES', {
-  SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-  SUPABASE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_SUPABASE_KEY,
-});
-
-
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
