04c6dc6d71a2a385eec10209db6f818abde9185d
Sean Stoves <sean@seanstoves.com>
2025-10-06 09:54:47 -0400

    updating env vars

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -18,6 +18,13 @@ const NotFound = () => {
         </a>
       </div>
     </div>
+  <pre>
+      {JSON.stringify({
+        url: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        key: process.env.NEXT_PUBLIC_SUPABASE_KEY ? '✔️ Loaded' : '❌ Missing',
+        nodeEnv: process.env.NODE_ENV,
+      }, null, 2)}
+    </pre>
   );
 };
 
