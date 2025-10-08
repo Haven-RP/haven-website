040abd58dbd9a994b0e64bdb9085e68c6322d79b
Sean Stoves <sean@seanstoves.com>
2025-10-08 15:22:04 -0400

    Update Tebex webstore identifier in site configuration

--- src/config/site.ts
+++ src/config/site.ts
@@ -39,7 +39,7 @@ export const siteConfig = {
   twitter: "",
   
   // Tebex Store
-  tebexWebstoreIdentifier: "store.haven-rp.com", // Your Tebex webstore URL
+  tebexWebstoreIdentifier: "havenrp.tebex.io", // Your Tebex webstore URL
   
   // Meta
   metaTitle: "HavenRP — Serious FiveM Roleplay",
